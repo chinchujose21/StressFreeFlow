e7e320783d0bae608fb86c22b93396389d55cd5a
Chinchu Jose <chinchu.jose@bytewavedigital.com>
2022-10-18 12:23:47 +0200

    First commit

---
+++ app/src/main/java/com/city/stressfreeflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.city.stressfreeflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
